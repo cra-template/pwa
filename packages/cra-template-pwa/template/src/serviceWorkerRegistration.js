2260be292e65816bf35034cfa4552788e946fc5e
Daniel Lovell <danielalovell@gmail.com>
2020-07-29 16:50:28 -0700

    change console.log to console.info

--- packages/cra-template-pwa/template/src/serviceWorkerRegistration.js
+++ packages/cra-template-pwa/template/src/serviceWorkerRegistration.js
@@ -39,7 +39,7 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
+          console.info(
             'This web app is being served cache-first by a service ' +
               'worker. To learn more, visit https://cra.link/PWA'
           );
@@ -67,7 +67,7 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
+              console.info(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://cra.link/PWA.'
               );
@@ -80,7 +80,7 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+              console.info('Content is cached for offline use.');
 
               // Execute callback
               if (config && config.onSuccess) {
@@ -120,7 +120,7 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      console.log('No internet connection found. App is running in offline mode.');
+      console.info('No internet connection found. App is running in offline mode.');
     });
 }
 
