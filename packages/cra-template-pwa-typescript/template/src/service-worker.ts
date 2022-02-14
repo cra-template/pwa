bb9e52b9df3b8ed196de851b8db4b3502b7996f9
christophe <chrisb@gmx.fr>
2022-02-14 14:41:16 +0100

    Update service-worker.ts

--- packages/cra-template-pwa-typescript/template/src/service-worker.ts
+++ packages/cra-template-pwa-typescript/template/src/service-worker.ts
@@ -27,7 +27,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+const fileExtensionRegexp = '/[^/?]+\\.[^/]+$';
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }: { request: Request; url: URL }) => {
@@ -50,15 +50,22 @@ registerRoute(
     // Return true to signal that we want to use the handler.
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL(`${process.env.PUBLIC_URL}/index.html`)
 );
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
+const imgExtensions = ['.png', '.svg', '.jpg', '.bmp'];
+
+const urlIsImage = (urlPathName) => {
+  const endsWithImage = imgExtensions.filter((extension) =>
+    urlPathName.endsWith(extension),
+  );
+  return endsWithImage?.length > 0;
+};
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
-  // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) => url.origin === self.location.origin && urlIsImage(url.pathname), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
     plugins: [
@@ -66,7 +73,7 @@ registerRoute(
       // least-recently used images are removed.
       new ExpirationPlugin({ maxEntries: 50 }),
     ],
-  })
+  }),
 );
 
 // This allows the web app to trigger skipWaiting via
